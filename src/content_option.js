7d480495a6a4adc99d93eac3dd4f5bf28fbba737
Vict3r-official <Vict3r.official@gmail.com>
2025-02-18 21:51:39 +0330

    content_option.js

--- src/content_option.js
+++ src/content_option.js
@@ -1,15 +1,15 @@
 const logotext = "JOHN";
 const meta = {
     title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    description: "I'm Victor Python Developer,currently Freelancing in Tehran",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Victor",
     animated: {
         first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        second: "I code Discord Bots",
+        third: "Enthusiastic about Red Team",
     },
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
